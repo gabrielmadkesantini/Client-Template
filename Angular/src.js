e498b9725d2cf60efe53b9d428defe2995b3b2d3
sophiawithph <sophiasgs11@gmail.com>
2023-12-20 11:39:58 -0300

    edit and insert appertly working

--- Angular/src.js
+++ Angular/src.js
@@ -1,32 +1,102 @@
 var app = angular.module("myApp", []);
 
-app.controller("modalController", ($scope) => {
-  $scope.show = false;
-  $scope.text = "";
-  $scope.ask = false;
-  $scope.buttonText = "";
+// app.directive("firstDirective", () => {
+//   return {
+//     require: "ngModel",
+//     link: () =>{
 
-  $scope.openModal = (text) => {
-    switch (text) {
-      case "delete":
-        $scope.text = "Deseja mesmo deletar este cliente?";
-        $scope.buttonText = "Sim";
-        $scope.ask = !$scope.ask;
-        break;
-      case "add":
-        $scope.text = "Criar cliente";
-        $scope.show = !$scope.show;
-        $scope.buttonText = "Criar Cliente";
-        break;
-      default:
-        $scope.text = "Editar Cliente";
-        $scope.show = !$scope.show;
-        $scope.buttonText = "Salvar";
+//     }
+//   };
+// });
+
+app.controller("modalController", [
+  "$scope",
+  "$rootScope",
+  ($scope, $rootScope) => {
+    $scope.show = false;
+    $scope.text = "";
+    $scope.ask = false;
+    $scope.buttonText = "";
+
+    $scope.openModal = (text, name) => {
+      $rootScope.name = name;
+      console.log(name);
+      switch (text) {
+        case "delete":
+          $scope.text = "Deseja mesmo deletar este cliente?";
+          $scope.buttonText = "Sim";
+          $scope.ask = !$scope.ask;
+          break;
+        case "add":
+          $scope.text = "Criar cliente";
+          $scope.show = !$scope.show;
+          $scope.buttonText = "Criar Cliente";
+          break;
+        default:
+          $scope.text = "Editar Cliente";
+          $scope.show = !$scope.show;
+          $scope.buttonText = "Salvar";
+      }
+    };
+  },
+]);
+
+app.controller("formController", [
+  "$scope",
+  "$rootScope",
+  ($scope, $rootScope) => {
+    $scope.showButtons = false;
+    $scope.master = {};
+    $scope.savedClients = [];
+    if ($scope.savedClients.length != 0) {
+      $scope.showButtons = !$scope.showButtons;
     }
-  };
-});
+    $scope.reset = () => {
+      $scope.user = {};
+      $scope.updateValues = {};
+    };
+    $scope.save = (user, updateValues) => {
+      console.log(updateValues);
 
-app.controller("formController", ($scope) => {
-  $scope.name = "";
-  console.log($scope.name);
-});
+      $scope.master = angular.copy(user);
+      if (updateValues) {
+        if (
+          user.name != updateValues.name ||
+          user.code != updateValues.code ||
+          user.address != updateValues.address
+        ) {
+          const specificIndex = $scope.savedClients.findIndex(
+            (e) => e.code === updateValues.code
+          );
+          console.log(user);
+          $scope.savedClients[specificIndex] = $scope.user;
+        } else {
+          if (updateValues == 0) {
+            $scope.savedClients.push($scope.user);
+          }
+        }
+      } else {
+        $scope.savedClients.push($scope.user);
+      }
+
+      $scope.reset();
+    };
+    $scope.openUpdate = () => {
+      console.log($rootScope.name);
+
+      if ($rootScope.name != "") {
+        $scope.user = $scope.savedClients.find(
+          (e) => e.name == $rootScope.name
+        );
+
+        $scope.updateValues = $scope.savedClients.find(
+          (e) => e.name == $rootScope.name
+        );
+
+        console.log($scope.user);
+      }
+    };
+
+    $scope.update = (name) => {};
+  },
+]);
